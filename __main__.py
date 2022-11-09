885932d2057fa37b591e4a637c1f05f62ef796e2
oneYAD <suskx7654321@gmail.com>
2022-11-09 18:23:31 +0300

    check, to be del"

--- __main__.py
+++ __main__.py
@@ -1736,6 +1736,9 @@ def save_runs_num(runs_num_file, runs_num, method, label, num_of_images, network
     with open(runs_num_file, "r") as f:
         runs_num_dict = json.load(f)
 
+    print("check this {}.{}.{}.{}.{}".format(method, network, label, num_of_images, precision))
+    logging.INFO("check this {}.{}.{}.{}.{}".format(method, network, label, num_of_images, precision))
+    print(os.path.basename(network))
     key = (method, os.path.basename(network), label, num_of_images, precision)
 
     if key in runs_num_dict:

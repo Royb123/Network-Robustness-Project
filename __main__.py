a632990cb152e1123763f418c1c920f8e3308a61
oneYAD <suskx7654321@gmail.com>
2022-11-18 11:42:10 +0200

    fix pool

--- __main__.py
+++ __main__.py
@@ -34,7 +34,7 @@ import time
 import json
 import traceback
 from multiprocessing import Process, Queue, Pool
-
+from itertools import product
 
 
 EPS_IS_LOWER = -1
@@ -565,11 +565,12 @@ def rng_search_all_epsilons(imgs_list, num_imgs, eps_file_path):
     return sorted_epsilons, runs_num
 
 def run_and_check_range_sizes_X_labels(labels, sizes):
-    for label in labels:
-        run_and_check_range_sizes(label, sizes)
+    with Pool(10) as p:
+        p.starmap(run_and_check_one_iteration, product(sizes, labels))
+
+    # for label in labels:
+    #     run_and_check_range_sizes(label, sizes)
 def run_and_check_range_sizes(label, sizes):
-    with Pool(5) as p:
-        p.map(run_and_check_one_iteration, map(lambda x: (x, str(label)), sizes))
 
     # for num_imgs in sizes:
     #

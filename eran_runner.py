6b125db7ba49524c629d6cebf6d58c50d4708cee
oneYAD <suskx7654321@gmail.com>
2022-11-18 10:29:50 +0200

    fix

--- eran_runner.py
+++ eran_runner.py
@@ -51,16 +51,6 @@ if is_tf_version_2:
     tf = tf.compat.v1
 
 
-def str2bool(v):
-    if v.lower() in ('yes', 'true', 't', 'y', '1'):
-        return True
-    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
-        return False
-    else:
-        raise argparse.ArgumentTypeError('Boolean value expected.')
-
-
-
 
 def parse_input_box(text):
     intervals_list = []

a57ea43fb39592e3e2e6f23789f29e1ad3a85990
oneYAD <suskx7654321@gmail.com>
2022-11-21 10:35:59 +0200

    test 1024

--- __main__.py
+++ __main__.py
@@ -582,11 +582,12 @@ def run_and_check_range_sizes_X_labels(sizes, labels):
 
 
 def run_and_check_one_iteration(num_imgs, label):
-    eps_file_path = './cheat_sheet_round_label_{}_indx_0_to_{}_precision_{}.csv'.format(str(label), str(num_imgs), str(PRECISION))
 
     start_time = time.time()
     user_logger.info("######################## start logging ########################")
-    images = load_dataset('mnist')
+    eps_file_path = './cheat_sheet_round_label_{}_indx_0_to_{}_precision_{}.csv'.format(str(label), str(num_imgs), str(PRECISION))
+
+    images = load_dataset('mnist') #TODO change using config.netname
     images.create_dict_to_eran()
     dataset = images.dict_to_eran
     imgs_list = dataset[label][:num_imgs-1]

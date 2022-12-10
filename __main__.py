03e3ddad7bdd8f9c29b9bc9c9c93be7f107e806c
yaelzink <zinkinyael@gmail.com>
2022-12-10 21:48:52 +0200

    same logic. remove naive cause redundant

--- __main__.py
+++ __main__.py
@@ -698,13 +698,13 @@ def main():
 
     sizes = [1024]
     labels = [2,]
-    methods = ["naive", "rng_binary_by_confidence", "rng_binary_by_random"]
+    methods = ["rng_binary_by_confidence", "rng_binary_by_random"]
     run_and_check_range_sizes_X_labels(sizes, labels, methods)
 
     time.sleep(60 * 60 * 5)
     sizes = [900,]
     labels = range(10)
-    methods = ["naive", "rng_binary_by_confidence"]
+    methods = [ "rng_binary_by_confidence"]
     run_and_check_range_sizes_X_labels(sizes, labels, methods)
 
 if __name__ == "__main__":

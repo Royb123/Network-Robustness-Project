04d1146deb6a5c3f96e4a230bbfb5b44f0095e1f
yaelzink <zinkinyael@gmail.com>
2022-12-21 09:57:27 +0200

    reverse sorted correctly

--- __main__.py
+++ __main__.py
@@ -707,9 +707,9 @@ def save_runs_num(runs_num_file, runs_num, options, network, precision=PRECISION
 def sort_img_correctly(indexed_imgs_list, num_imgs, eps_file_path):
     eps_arr, _ = load_cheat_eps_from_csv(eps_file_path, num_imgs)
     user_logger.info("sort_img_correctly: loaded {}".format(eps_arr))
-    sorted_eps_arr = sorted(eps_arr, key=lambda eps: eps[1])
-    user_logger.info("sort_img_correctly: sorted epsilons {}".format(sorted_eps_arr))
-    sorted_imgs = sorted(indexed_imgs_list, key=lambda img: sorted_eps_arr[img.index][0])
+    sorted_by_ind_eps_arr = sorted(eps_arr, key=lambda eps: eps[1])
+    user_logger.info("sort_img_correctly: sorted epsilons {}".format(sorted_by_ind_eps_arr))
+    sorted_imgs = sorted(indexed_imgs_list, key=lambda img: sorted_by_ind_eps_arr[img.index][0], reverse=True)
     user_logger.info("sort_img_correctly: sorted imgs {}".format([img.index for img in sorted_imgs]))
 
     return sorted_imgs

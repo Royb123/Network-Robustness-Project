5ab0860dd07785aa24e68845c12f1bcce324b61d
yaelzink <zinkinyael@gmail.com>
2023-01-02 23:17:12 +0200

    run only 9

--- __main__.py
+++ __main__.py
@@ -873,7 +873,7 @@ def main():
     parse_args()
 
     sizes = [1024, ]
-    labels = [6, 9]
+    labels = [9, ]
     methods = ["ignore_mistake_control", "ignore"]
     score_funcs = ["naive_and_sorted_correctly", "random", "confidence"]
     run_and_check_range_sizes_X_labels(sizes, labels, methods, score_funcs)

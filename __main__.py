5714fe0434fa88e635ee570a287a683837a04396
yaelzink <zinkinyael@gmail.com>
2023-01-08 11:27:10 +0200

    rerun after stuck dana 2

--- __main__.py
+++ __main__.py
@@ -872,25 +872,31 @@ def main():
     """
     parse_args()
 
-    sizes = [1024, ]
-    labels = [2, 4, 5, 7, 8,]
-    methods = ["ignore_mistake_control"]
-    score_funcs = ["naive", "random", "confidence"]
+    sizes = [2048, 128]
+    labels = [6, 9]
+    methods = ["ignore_mistake_control", "ignore"]
+    score_funcs = ["naive_and_sorted_correctly", "random", "confidence"]
     run_and_check_range_sizes_X_labels(sizes, labels, methods, score_funcs)
 
-    sizes = [16 * 2 ** i for i in range(6)]
-    labels = [3, 6]
-    methods = ["ignore_mistake_control"]
-    score_funcs = ["naive", "confidence"]
+    sizes = [128]
+    labels = [3]
+    methods = ["ignore_mistake_control", "ignore"]
+    score_funcs = ["naive_and_sorted_correctly", "random", "confidence"]
     run_and_check_range_sizes_X_labels(sizes, labels, methods, score_funcs)
 
-
-    # sizes = [16 * 2 ** i for i in range(6)]
-    # labels = [3, 6]
-    # methods = ["ignore_mistake_control",]
-    # score_funcs = ["naive", "confidence"]
+    # dana 1
+    # sizes = [1024, ]
+    # labels = [2, 4, 5, 7, 8,]
+    # methods = ["ignore_mistake_control"]
+    # score_funcs = ["naive", "random", "confidence"]
     # run_and_check_range_sizes_X_labels(sizes, labels, methods, score_funcs)
     #
+    # sizes = [16 * 2 ** i for i in range(6)]
+    # labels = [3, 6]
+    # methods = ["ignore_mistake_control"]
+    # score_funcs = ["naive", "confidence"]
+    # run_and_check_range_sizes_X_labels(sizes, labels, methods, score_funcs)
+
 
 if __name__ == "__main__":
     try:

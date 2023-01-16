e31b040e7748adab9ac9446894f0c881bed4ff89
yaelzink <zinkinyael@gmail.com>
2023-01-16 11:31:01 +0200

    new run in dana 2 check tendency with bigger size

--- __main__.py
+++ __main__.py
@@ -872,19 +872,27 @@ def main():
     """
     parse_args()
 
-    # new run in dana 1 withe different network
-
+    #new run in dana 1 withe different network
     sizes = [1024, ]
-    labels = [2,4]
+    labels = [7]
     methods = ["ignore_mistake_control", "ignore"]
     score_funcs = ["naive_and_sorted_correctly", "random", "confidence"]
     run_and_check_range_sizes_X_labels(sizes, labels, methods, score_funcs)
 
-    # sizes = [2048, 128]
-    # labels = [6, 9]
-    # methods = ["ignore_mistake_control", "ignore"]
-    # score_funcs = ["naive_and_sorted_correctly", "random", "confidence"]
-    # run_and_check_range_sizes_X_labels(sizes, labels, methods, score_funcs)
+    #new run in dana 2 check tendency with bigger size
+
+    sizes = [2048, 4096]
+    labels = [6]
+    methods = ["ignore_mistake_control"]
+    score_funcs = ["naive", "confidence"]
+    run_and_check_range_sizes_X_labels(sizes, labels, methods, score_funcs)
+
+    sizes = [4096]
+    labels = [3]
+    methods = ["ignore_mistake_control"]
+    score_funcs = ["naive", "confidence"]
+    run_and_check_range_sizes_X_labels(sizes, labels, methods, score_funcs)
+
     #
     # sizes = [128]
     # labels = [3]
